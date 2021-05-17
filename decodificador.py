bb4698844537d2760a0884fb5069c4222b770fa2
Milena Ruy <43274554+2621@users.noreply.github.com>
2021-05-17 18:39:36 -0300

    Update decodificador.py

--- decodificador.py
+++ decodificador.py
@@ -1,9 +1,38 @@
-from decod_metodos import image_scanner, huffman_decod
+import json
+import numpy as np
 
 pgm_list = [
     "images/lena.pgm",
     "images/baboon.pgm"
 ]
+
+def get_image_info(name):
+    f = open(f"{name}.huff", "r")
+    content = f.read()
+    info = json.loads(content)
+    return info["codewords"], info["dimensions"], info["vmax"], info["code"]
+
+def dictionary(codewords):
+    decoder = {}
+    for key in codewords:
+        decoder[codewords[key]] = key
+    return decoder
+
+def decode(code, decoder):
+    max_code_length = 0
+    for key in decoder:
+        if max_code_length < len(key):
+            max_code_length = len(key)
+
+    decoded = []
+    while (len(code) > 0):
+        for i in range(1, max_code_length + 1):
+            if decoder.get(code[:i], False):
+                decoded.append(int(decoder[code[:i]]))
+                code = code[i:]
+                break
+    
+    return decoded
 
 def save_decod(name, image, dimensions, vmax):
     image_copy = image.copy()
@@ -19,7 +48,7 @@ def save_decod(name, image, dimensions, vmax):
     print(f"{name}.huff.pgm saved")
 
 for pgm in pgm_list:
-    codewords, dimensions, vmax, code = image_scanner.get_image_info(pgm)
-    decod = huffman_decod.dictionary(codewords)
-    image = huffman_decod.decode(code, decoder)
+    codewords, dimensions, vmax, code = get_image_info(pgm)
+    decod = dictionary(codewords)
+    image = decode(code, decoder)
     save_decod(pgm, image, dimensions, vmax)
